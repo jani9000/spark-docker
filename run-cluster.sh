9af5cf321fde417a2e9d11ac12a578dcab321087
Jani Leppanen <jleppane@adobe.com>
2015-02-05 16:08:59 -0800

    Add HDFS, where namenode runs in its own container and Spark slaves double as datanodes.

--- run-cluster.sh
+++ run-cluster.sh
@@ -4,7 +4,7 @@ set -e
 
 SLAVE_COUNT=2
 
-# Run slaves.
+echo "Starting spark slave containers."
 
 for((i=1; i<=SLAVE_COUNT; i++))
 do
@@ -13,12 +13,15 @@ do
     -p 22 \
     -p 7078 \
     -p 8081 \
+    -p 50075 \
+    -p 50010 \
+    -p 50020 \
     -d \
     -v $(pwd)/data:/var/spark/data \
     jani9000/spark-docker /sbin/my_init -- sh -c "tail -f /dev/null"
 done
 
-# Run master and link slaves.
+echo "Starting spark master container."
 
 function slave_links {
   for((i=1; i<=SLAVE_COUNT; i++))
@@ -37,12 +40,33 @@ docker run \
   -v ~/apps:/var/spark/apps \
   jani9000/spark-docker /sbin/my_init -- sh -c "cd /usr/local/spark && sbin/start-master.sh && tail -f /dev/null"
 
+echo "Starting Hadoop namenode container."
+
+docker run \
+  --name hadoopnamenode \
+  -p 50070 \
+  -p 9000 \
+  -d \
+  $(slave_links) \
+  jani9000/spark-docker /sbin/my_init -- sh -c "tail -f /dev/null"
+
+docker exec -i hadoopnamenode sh -c "rm \$HADOOP_CONF_DIR/slaves"
+
+echo "Configuring slaves file for Spark."
+
 for((i=1; i<=SLAVE_COUNT; i++))
 do
   docker exec -i sparkmaster sh -c "echo sparkslave$i >> /usr/local/spark/conf/slaves"
 done
 
-# Create master ssh key and distribute public key to slaves.
+echo "Configuring slaves file for Hadoop."
+
+for((i=1; i<=SLAVE_COUNT; i++))
+do
+  docker exec -i hadoopnamenode sh -c "echo sparkslave$i >> /usr/local/hadoop/etc/hadoop/slaves"
+done
+
+echo "Configuring passwordless SSH from Spark master to Spark slaves."
 
 docker exec -i sparkmaster sh -c "ssh-keygen -t rsa -N '' -f /root/.ssh/id_rsa"
 
@@ -56,9 +80,41 @@ do
   docker exec -i sparkslave$i sh -c "cat >> /root/.ssh/authorized_keys" < tmp/sparkmaster.pub/id_rsa.pub
   docker exec -i sparkslave$i sh -c "dpkg-reconfigure openssh-server" # Create host keys.
   docker exec -i sparkslave$i sh -c "/usr/sbin/sshd"
+done
+
+echo "Adding Spark master to slaves' hosts file."
+
+for((i=1; i<=SLAVE_COUNT; i++))
+do
   docker exec -i sparkslave$i sh -c "echo $sparkmaster_ip $sparkmaster_hostname >> /etc/hosts"
 done
 
-# Ask master to start slaves.
+echo "Adding Hadoop namenode to hosts files on HDFS cluster."
+
+hadoop_namenode_ip=`docker inspect -f '{{ .NetworkSettings.IPAddress }}' hadoopnamenode`
+
+docker exec -i hadoopnamenode sh -c "echo $hadoop_namenode_ip hadoopnamenode >> /etc/hosts"
+
+for((i=1; i<=SLAVE_COUNT; i++))
+do
+  docker exec -i sparkslave$i sh -c "echo $hadoop_namenode_ip hadoopnamenode >> /etc/hosts"
+done
+
+echo "Formatting HDFS on Hadoop namenode."
+
+docker exec -i hadoopnamenode sh -c "\$HADOOP_CONF_DIR/hadoop-init.sh"
+
+echo "Starting HDFS datanodes."
+
+for((i=1; i<=SLAVE_COUNT; i++))
+do
+  docker exec -i sparkslave$i sh -c "export USER=root; \$HADOOP_PREFIX/sbin/hadoop-daemon.sh --config \$HADOOP_CONF_DIR --script hdfs start datanode"
+done
+
+echo "Starting HDFS namenode."
+
+docker exec -i hadoopnamenode sh -c "export USER=root; \$HADOOP_PREFIX/sbin/hadoop-daemon.sh --config \$HADOOP_CONF_DIR --script hdfs start namenode"
+
+echo "Asking Spark master to start slaves."
 
 docker exec -i sparkmaster sh -c "cd /usr/local/spark && sbin/start-slaves.sh"
